60d2a249ab139a281b1208c4f51d22115851c4d0
Might1331 <prataps801@gmail.com>
2023-07-06 17:54:04 +0530

    Unused imports removed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use typedb_client::{
     concept::{Attribute, Concept, Value},
-    error::ConnectionError,
-    Connection, DatabaseManager, Error, Options, Session,
+    Connection, DatabaseManager, Session,
     SessionType::{Data, Schema},
     TransactionType::{Read, Write},
-    Credential, Database
 };
-use futures::{executor::block_on, StreamExt};
+use futures::{StreamExt};
 use  std::fs;
 use text_io::read;
 
@@ -20,7 +18,7 @@ async fn load_data(connection: Connection)->std::io::Result<()>{
     let data=fs::read_to_string("./src/data.tql")?;    
     let databases = DatabaseManager::new(connection.clone());
     if databases.contains(TEST_DATABASE).await.unwrap()==false {
-        databases.create(TEST_DATABASE).await;    // insert data
+        databases.create(TEST_DATABASE).await.unwrap();    // insert data
         let session = Session::new(databases.get(TEST_DATABASE).await.unwrap(), Data).await.unwrap();
         let transaction = session.transaction(Write).await.unwrap();
         let _ = transaction.query().insert(data.as_str());
@@ -39,7 +37,7 @@ async fn load_schema(connection: Connection)->std::io::Result<()>{
     // query_options(con.clone()).await.unwrap();
     let databases = DatabaseManager::new(connection.clone());
     if databases.contains(TEST_DATABASE).await.unwrap()==false {
-        databases.create(TEST_DATABASE).await;
+        databases.create(TEST_DATABASE).await.unwrap();
         // define schema
         let session = Session::new(databases.get(TEST_DATABASE).await.unwrap(), Schema).await.unwrap();
         let transaction = session.transaction(Write).await.unwrap();
@@ -80,7 +78,7 @@ async fn query2(connection: Connection,q: String)->std::io::Result<()>{
     let session = Session::new(databases.get(TEST_DATABASE).await.unwrap(), Data).await.unwrap();
     println!("::Q2::");
     let transaction = session.transaction(Read).await.unwrap();
-    let mut answer = transaction.query().match_aggregate(q.as_str()).await.unwrap();
+    let answer = transaction.query().match_aggregate(q.as_str()).await.unwrap();
     println!("Answe for Q2:  {}\n",answer.into_i64());
     Ok(())
 }
@@ -113,46 +111,46 @@ async fn runIO(connection: Connection){
     if qtype==1{
         println!("Enter the name of raw_food x : ");
         let inp: String=read!();
-        let mut x=inp.as_str();
-        let mut p0="match $rf isa raw_food, has name \"";
-        let mut p1="\";
+        let x=inp.as_str();
+        let p0="match $rf isa raw_food, has name \"";
+        let p1="\";
         $is_i (raw_food: $rf,dish: $d) isa is_ingredient;
         $sl (seller: $m,product: $d) isa sells;
         $m has name $n,has call_number $c;
         get $n;";
         let mut f="".to_owned();
         f=f+p0+x+p1;
-        query1(connection,f).await;
+        query1(connection,f).await.unwrap();
     }else if qtype==2{
-        let mut p0="match 
+        let p0="match 
         $m2 isa menu, has is_vegetarian false,has name $n2;
         $d2 isa dish, has is_vegetarian false;
         $sp2 (restaurant: $m2,$d2) isa speciality;
         get $m2;count;";
         let mut f="".to_owned();
         f=f+p0;
-        query2(connection,f).await;
+        query2(connection,f).await.unwrap();
     }else if qtype==3{
         println!("Enter the avg_rating of Restraunt r : ");
         let inp0: String=read!();
-        let mut r=inp0.as_str();
+        let r=inp0.as_str();
         println!("Enter the threshold price for raw_ingredient p : ");
         let inp1: String=read!();
-        let mut p=inp1.as_str();
-        let mut p0="match
+        let p=inp1.as_str();
+        let p0="match
         $m3 isa menu, has avg_rating>";
-        let mut p1=",has name $mn3;
+        let p1=",has name $mn3;
         $d3 isa dish, has name $dn3;
         $sl3 (seller: $m3,product: $d3) isa sells, has price $p3;
         $p3>";
-        let mut p2=";
+        let p2=";
         $rf3 isa raw_food,has name $rfn3;
         $isn3 (raw_food: $rf3,$d3) isa is_ingredient;
         get $rfn3;";
         let mut f="".to_owned();
         f=f+p0+r+p1+p+p2;
 
-        query3(connection,f).await;
+        query3(connection,f).await.unwrap();
     }else{
         println!("Query entered is not 1,2 or 3\n");
     }
@@ -173,5 +171,5 @@ async fn mymain()->std::io::Result<()>{
 async fn main(){
     // let r=mymain();
     // block_on(r);
-    mymain().await;
+    mymain().await.unwrap();
 }
